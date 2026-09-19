4540e1e7ac68f8ade79b99401cdde88886bbbdf1
Ed Baker <edwbaker@gmail.com>
2026-09-19 12:07:46 +0100

    Link recordings and trait values to their taxa
    links.csv now also links recordings to the taxa they are of (their species,
    6,751 links) and trait values to the taxa of their traits node (5,513), both
    with IAO "is about", as references already are, so everything about a taxon
    can be found with one predicate. Links to taxa that no longer exist are left
    out, which also drops 2 of the references' links. There are 19,595 links in
    all.

--- export/links.sql
+++ export/links.sql
@@ -3,7 +3,9 @@
 -- Records are identified by their type (data module) and their id at
 -- bio.acousti.ca: references and recordings by node id, traits by the id of
 -- their field collection item (the traitID of traits.txt) and taxa by term id.
--- Only links between published nodes are given.
+-- Only links between published nodes, and to taxa that still exist, are given.
+-- References, recordings and trait values are all about taxa (IAO "is about"),
+-- so everything about a taxon can be found by one predicate.
 --
 -- What a reference contains (content) and its topics are given as Drupal term
 -- ids; links.R replaces them with the vocab.audioblast.org terms for them.
@@ -15,6 +17,7 @@ SELECT 'references' AS subject_type, tn.entity_id AS subject_id,
   NULL AS content, NULL AS topic, NULL AS remarks
 FROM field_data_field_taxonomic_name tn
 JOIN node n ON n.nid = tn.entity_id AND n.type = 'biblio' AND n.status = 1
+JOIN taxonomy_term_data t ON t.tid = tn.field_taxonomic_name_tid
 WHERE tn.entity_type = 'node' AND tn.deleted = 0
 
 UNION ALL
@@ -29,7 +32,30 @@ JOIN field_data_field_biblio_contents bc
   ON bc.entity_type = 'field_collection_item' AND bc.entity_id = c.field_contents_value AND bc.deleted = 0
 JOIN field_data_field_taxonomic_name tn
   ON tn.entity_type = 'field_collection_item' AND tn.entity_id = c.field_contents_value AND tn.deleted = 0
+JOIN taxonomy_term_data t ON t.tid = tn.field_taxonomic_name_tid
 WHERE c.entity_type = 'node' AND c.deleted = 0
+
+UNION ALL
+
+-- Recordings of taxa (their species)
+SELECT 'recordings', s.entity_id, 'http://purl.obolibrary.org/obo/IAO_0000136',
+  'taxa', s.field_species_tid, NULL, NULL, NULL
+FROM field_data_field_species s
+JOIN node n ON n.nid = s.entity_id AND n.type = 'recording' AND n.status = 1
+JOIN taxonomy_term_data t ON t.tid = s.field_species_tid
+WHERE s.entity_type = 'node' AND s.deleted = 0
+
+UNION ALL
+
+-- Trait values of taxa: the taxa of the traits node that a value belongs to
+SELECT 'traits', h.field_bioacoustic_traits_value, 'http://purl.obolibrary.org/obo/IAO_0000136',
+  'taxa', tn.field_taxonomic_name_tid, NULL, NULL, NULL
+FROM field_data_field_bioacoustic_traits h
+JOIN node n ON n.nid = h.entity_id AND n.status = 1
+JOIN field_data_field_taxonomic_name tn
+  ON tn.entity_type = 'node' AND tn.entity_id = h.entity_id AND tn.deleted = 0
+JOIN taxonomy_term_data t ON t.tid = tn.field_taxonomic_name_tid
+WHERE h.entity_type = 'node' AND h.deleted = 0
 
 UNION ALL
 
@@ -59,6 +85,7 @@ UNION ALL
 SELECT 'taxa', f.entity_id, 'http://rs.tdwg.org/dwc/terms/namePublishedInID',
   'references', f.field_reference_nid, NULL, NULL, p.field_page_number_value
 FROM field_data_field_reference f
+JOIN taxonomy_term_data t ON t.tid = f.entity_id
 JOIN node b ON b.nid = f.field_reference_nid AND b.type = 'biblio' AND b.status = 1
 LEFT JOIN field_data_field_page_number p
   ON p.entity_type = 'taxonomy_term' AND p.entity_id = f.entity_id AND p.deleted = 0
